2ac42e4e6df173f536567f0cf880d932ddff5a99
blitzpc17 <remizeroaw117@gmail.com>
2023-12-13 16:06:05 -0600

    Busqueda Clientes

---
+++ CapaNegocios/Logicas/Busquedas/BusquedaClientesLogica.cs
@@ -0,0 +1,100 @@
+﻿using CapaDatos.ADO.Venta;
+using CapaDatos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocios.Logicas.Busquedas
+{
+    public class BusquedaClientesLogica
+    {
+
+        private ClienteADO contexto;
+        public List<clsCLIENTE> LstCliente;
+        public List<clsCLIENTE> LstClienteAux;
+
+        public int index = -1;
+        public int indexAux = -1;
+        public int Column = 0;
+
+        public BusquedaClienteLogica()
+        {
+            contexto = new ClienteADO();
+        }
+
+        public void ListarRegistros()
+        {
+            LstCliente = contexto.ListarClientes();
+        }
+
+        public clsCLIENTE ObtenerRegistro(int id)
+        {
+            return contexto.ObtenerClienteData(id);
+        }
+
+        public bool Filtrar(int column, string termino)
+        {
+            if (LstClienteAux == null) LstClienteAux = LstCliente;
+
+            switch (column)
+            {
+                case 1:
+                    index = LstClienteAux.FindIndex(x => x.Clave.StartsWith(termino));
+                    break;
+                case 2:
+                    index = LstClienteAux.FindIndex(x => x.Nombre.ToString().StartsWith(termino));
+                    break;
+                case 6:
+                    index = LstClienteAux.FindIndex(x => x.Estado.StartsWith(termino));
+                    break;
+                case 7:
+                    index = LstClienteAux.FindIndex(x => x.RFC.StartsWith(termino));
+                    break;
+                case 8:
+                    index = LstClienteAux.FindIndex(x => x.RazonSocial.StartsWith(termino));
+                    break;
+
+
+                default:
+                    index = -1;
+                    break;
+
+            }
+
+            return (index >= 0);
+
+        }
+
+        public void Ordenar(int column)
+        {
+            switch (column)
+            {
+
+                case 1:
+                    LstClienteAux = LstCliente.OrderBy(x => x.Clave).ThenBy(x => x.Nombres).ThenBy(x => x.Estado).ToList();
+                    break;
+                case 2:
+                    LstClienteAux = LstCliente.OrderBy(x => x.Nombres).ThenBy(x => x.Clave).ThenBy(x => x.Nombres).ThenBy(x => x.Estado).ToList();
+                    break;
+                case 6:
+                    LstClienteAux = LstCliente.OrderBy(x => x.Estado).ThenBy(x => x.Nombres).ThenBy(x => x.Clave).ThenBy(x => x.Nombres).ToList();
+                    break;
+                case 7:
+                    LstClienteAux = LstCliente.OrderBy(x => x.RFC).ThenBy(x => x.Nombres).ThenBy(x => x.Clave).ThenBy(x => x.Nombres).ToList();
+                    break;
+                case 8:
+                    LstClienteAux = LstCliente.OrderBy(x => x.RazonSocial).ThenBy(x => x.Nombres).ThenBy(x => x.Clave).ThenBy(x => x.Nombres).ToList();
+                    break;
+
+                default:
+                    LstClienteAux = LstCliente.OrderBy(x => x.Clave).ThenBy(x => x.Nombres).ThenBy(x => x.Estado).ToList();
+                    break;
+
+            }
+        }
+
+
+    }
+}
--- Presentacion/Busquedas/busCLIENTES.cs
+++ Presentacion/Busquedas/busCLIENTES.cs
@@ -1,4 +1,6 @@
-﻿using Presentacion.Utilidades;
+﻿using CapaDatos.Entidades;
+using CapaNegocios.Logicas.Busquedas;
+using Presentacion.Utilidades;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,17 +15,110 @@ namespace Presentacion.Busquedas
 {
     public partial class busClientes : Form
     {
+        private BusquedaClientesLogica contexto;
+        public clsCLIENTE ObjEntidad;
+        private int rowIndexSeleccionado = -1;
         public busClientes()
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
             WindowState = FormWindowState.Normal;
+        }        
+
+        private void InicializarForm()
+        {
+            contexto = new BusquedaClientesLogica();
+            Listar();
+            ordenar(1);
+        }
+
+        public void Listar()
+        {
+            contexto.ListarRegistros();
+        }
+
+        private void filtrar(int column, string termino)
+        {
+            if (contexto.Filtrar(column, termino))
+            {
+                contexto.indexAux = contexto.index;
+                dgvRegistros.Rows[contexto.index].Cells[column].Selected = true;
+                dgvRegistros.FirstDisplayedScrollingRowIndex = contexto.index;
+            }
+        }
+
+        private void ordenar(int column)
+        {
+            txtBuscar.Clear();
+            txtBuscar.Focus();
+            contexto.Ordenar(column);
+            dgvRegistros.DataSource = contexto.LstClienteAux;
+            Apariencias();
+        }
+
+        private void Apariencias()
+        {
+            dgvRegistros.Columns[0].Visible = false;
+            dgvRegistros.Columns[1].HeaderText = "CLAVE";
+            dgvRegistros.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dgvRegistros.Columns[2].HeaderText = "MARCA";
+            dgvRegistros.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dgvRegistros.Columns[3].HeaderText = "PROVEEDOR";
+            dgvRegistros.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dgvRegistros.Columns[4].HeaderText = "ESTADO";
+            dgvRegistros.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dgvRegistros.Columns[5].Visible = false;
+
+            tsTotalRegistros.Text = contexto.LstCliente.Count.ToString("N0");
+
         }
 
         private void busClientes_Shown(object sender, EventArgs e)
         {
             ThemeConfig.ThemeControls(this);
             this.MaximizeBox = false;
+        }
+
+        private void btnSeleccionar_Click(object sender, EventArgs e)
+        {
+            if (dgvRegistros.DataSource == null) return;
+            if (rowIndexSeleccionado == -1) return;
+            rowIndexSeleccionado = (int)dgvRegistros.CurrentRow.Cells[0].Value;
+            ObjEntidad = contexto.ObtenerRegistro(rowIndexSeleccionado);
+            Close();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            if (dgvRegistros.Rows.Count <= 0) return;
+
+            if (string.IsNullOrEmpty(txtBuscar.Text))
+            {
+                contexto.index = -1;
+                return;
+            }
+
+            filtrar(contexto.Column, txtBuscar.Text);
+        }
+
+        private void dgvRegistros_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (contexto.Column == e.ColumnIndex) return;
+            contexto.Column = e.ColumnIndex;
+            ordenar(contexto.Column);
+        }
+
+        private void dgvRegistros_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvRegistros.DataSource == null) return;
+            rowIndexSeleccionado = (int)dgvRegistros.CurrentRow.Cells[0].Value;
+            ObjEntidad = contexto.ObtenerRegistro(rowIndexSeleccionado);
+            Close();
         }
     }
 }
